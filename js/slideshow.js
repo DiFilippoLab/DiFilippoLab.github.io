1c8013a79613f24ab48fa904acaf02fded740616
DiFilippoLab <95710949+DiFilippoLab@users.noreply.github.com>
2021-12-10 02:04:45 -0500

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,7 +3,7 @@ carousel();
 
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("research-automated-thumbnail")[0];
+  var x = document.getElementsByClassName("research-automated-thumbnail");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
